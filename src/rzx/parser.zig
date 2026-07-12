e485a623bd3c38e187cabb648f0382504cd78c3a
Renzix <DanielDeBruno@renzix.com>
2026-07-12 13:52:01 -0400

    updated parser to work with basic simple_command implementation

--- src/rzx/parser.zig
+++ src/rzx/parser.zig
@@ -7,7 +7,6 @@ pub const TokenType = enum {
     WORD,
     ASSIGNMENT_WORD,
     NAME,
-    CONTROL_OPERATOR,
     NEWLINE,
     IO_NUMBER,
     IO_LOCATION,
@@ -27,7 +26,7 @@ fn Charset(comptime chars: []const u8) [256]bool {
 
 const WordChars = Charset("abcdefghijklmnopqrstuvwxyz"
     ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    ++ "0123456789" ++ "_${}\"'");
+    ++ "0123456789" ++ "_${}\"'.");
 
 const AssignmentChars = Charset("abcdefghijklmnopqrstuvwxyz"
     ++ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -62,60 +61,194 @@ pub const Parser = struct {
         self.code = str;
         // log("str: {}, content: {}\n", .{str.len, self.content.len});
         // std.debug.assert(self.content_len == str.len);
-        var foundToken = (self.code.len != 0);
-        while (self.code.len > self.i) {
-            foundToken = self.lexToken();
-        }
-    }
-    fn lexToken(self: *Parser) bool {
-        var ch = self.code[self.i];
+        // var foundToken = (self.code.len != 0);
 
-        while (self.i < self.code.len) {
-            ch = self.code[self.i];
-            switch (ch) {
-                'a'...'z', 'A'...'Z' => {
-                    if(self.lexWord()) return true;
-                    if(self.lexAssignment()) return true;
-                },
-                else => {
-                    log("Unknown char {c}\n", .{ch});
-                },
-            }
-            self.i+=1;
+        _ = self.parseSimpleCommand();
+        // while (self.code.len > self.i) {
+        //     foundToken = self.lexToken();
+        // }
+    }
+    fn parseSimpleCommand(self: *Parser) bool {
+        self.first_token = true;
+        if(self.parseCmdPrefix()) {
+            self.first_token = false;
+        }
+        if(self.parseCmdName()) {
+            self.first_token = false;
+        }
+        if(self.parseCmdSuffix()) {}
+        return !self.first_token;
+        // switch (ch) {
+        //     '|', '>', '<'
+        //     'a'...'z', 'A'...'Z' => {
+        //         if(self.lexWord()) {
+        //             // if first then set to cmd_name
+        //             // else set to cmd suffex
+        //         }
+        //         if(self.lexAssignment()) return true;
+        //     },
+        //     else => {
+        //         log("Unknown char {c}\n", .{ch});
+        //     },
+        // }
+    }
+
+    fn parseCmdPrefix(self: *Parser) bool {
+        var found = false;
+        while(true) {
+            _ = self.skipWhitespace();
+            if (self.lexAssignment()) { found=true; continue; }
+            if (self.parseIoRedirect()) { found=true; continue; }
+            break;
+        }
+        return found;
+    }
+
+    fn parseCmdSuffix(self: *Parser) bool {
+        var found = false;
+        while(true) {
+            _ = self.skipWhitespace();
+            if (self.lexWord()) { found=true; continue; }
+            if (self.parseIoRedirect()) { found=true; continue; }
+            break;
+        }
+        return found;
+    }
+
+    // @TODO(DeBruno): Add io_here and maybe io_location
+    fn parseIoRedirect(self: *Parser) bool {
+        // var found = false;
+        _ = self.skipWhitespace();
+        // self.lexIoNumber();
+        _ = self.lexIoFile();
+        return false;
+    }
+
+    fn lexIoFile(self: *Parser) bool {
+        self.start = self.i;
+        if (self.i >= self.code.len) return false;
+        switch(self.code[self.i]) {
+            '>' => {
+                if (self.i+1 < self.code.len) {
+                    switch (self.code[self.i+1]) {
+                        '&' => { // GREATAND >&
+                            // @TODO(Renzix): Implement
+                            @panic("GREATAND");
+                        },
+                        '>' => { // DGREAT >>
+                            // @TODO(Renzix): Implement
+                            @panic("DGREAT");
+                        },
+                        '|' => { // CLOBBER >|
+                            // @TODO(Renzix): Implement
+                            @panic("Clobber");
+                        },
+                        else => { // '>' or invalid
+                            log("Found >: >\n", .{});
+                            self.i += 1;
+                            _ = self.skipWhitespace();
+                            // expected filename, io_rediect with no filename
+                            if(!self.lexWord())
+                                @panic("expected filename, io_redirect with no filename");
+                            return true;
+                        }
+                    }
+                } else {
+                    // parse error, io_redirect with no filename
+                    // log("index: {}\n",.{self.i});
+                    // log("code.len: {}\n",.{self.code.len});
+                    @panic("parse error, io_redirect with no filename");
+                }
+            },
+            '<' => {
+                if (self.i+1 < self.code.len) {
+                    switch (self.code[self.i+1]) {
+                        '&' => { // LESSAND <&
+                            @panic("LESSAND");
+                        },
+                        '>' => { // LESSGREAT <>
+                            @panic("LESSGREAT");
+                        },
+                        else => { // '<' or invalid
+                            log("Found <: <\n", .{});
+                            self.i += 1;
+                            _ = self.skipWhitespace();
+                            // expected filename, io_rediect with no filename
+                            if(!self.lexWord())
+                                @panic("expected filename, io_redirect with no filename");
+                            return true;
+                        }
+                    }
+                }
+            },
+            else => {
+                return false;
+            },
         }
         return false;
     }
+
+    fn parseCmdName(self: *Parser) bool {
+        return self.lexWord();
+    }
+
     fn lexWord(self: *Parser) bool {
         self.start = self.i;
+        var found=false;
         while(self.i < self.code.len and WordChars[self.code[self.i]]) {
             self.i += 1;
+            found=true;
         }
+        if(!found) {
+            self.i = self.start;
+            return false;
+        }
+        if (self.i < self.code.len and self.code[self.i]=='=') {
+            self.i = self.start;
+            return false;
+        }
+
         const len = self.i - self.start;
         @memcpy(self.content[0..len], self.code[self.start..self.i]);
         self.content_len = len;
-        if (self.i < self.code.len and self.code[self.i]=='=') {
-            return false;
-        }
-        log("Found Word\n", .{});
+        log("Found Word: {s}\n", .{self.content[0..len]});
         self.token = TokenType.WORD;
         return true;
     }
     fn lexAssignment(self: *Parser) bool {
         self.start = self.i;
+        var found=false;
+        var eql_index: ?usize = null;
         while(self.i < self.code.len and AssignmentChars[self.code[self.i]]) {
+            if (eql_index==null and self.code[self.i]=='=') eql_index=self.i;
             self.i += 1;
+            found=true;
+        }
+        if(!found) {
+            self.i = self.start;
+            return false; // no characters
+        }
+        if (eql_index==null) {
+            self.i = self.start;
+            return false; // couldnt find = sign
         }
         const len = self.i - self.start;
         @memcpy(self.content[0..len], self.code[self.start..self.i]);
         self.content_len = len;
-        if (self.i < self.code.len and self.code[self.i]=='=') {
-            return false;
-        }
-        log("Found Assignment\n", .{});
+        log("Found Assignment: {s}\n", .{self.content[0..len]});
         self.token = TokenType.ASSIGNMENT_WORD;
         return true;
     }
-
+    fn contentClear(self: *Parser) void {
+        @memset(&self.content, 0);
+        self.content_len = 0;
+    }
+    fn skipWhitespace(self: *Parser) usize {
+        std.debug.assert(self.quoted==Quoted.QUOTED_NONE);
+        const start = self.i;
+        while (self.i<self.code.len and self.code[self.i]==' ') self.i+=1;
+        return self.i - start;
+    }
 };
 
 
