5c3f74c4f72557f6172f9aedbce429b34dbe38a0
Renzix <DanielDeBruno@renzix.com>
2026-08-07 23:32:17 -0400

    properly storing field_width and precision

--- src/rzvm/builtins/printf.zig
+++ src/rzvm/builtins/printf.zig
@@ -61,6 +61,7 @@ const SubErr = error{
     OutOfMemory,
     ParseInt,
     ParseUInt,
+    InvalidConversion,
 };
 
 const Subsitution = struct {
@@ -68,11 +69,14 @@ const Subsitution = struct {
     field_width: u32 = 0,    // amount of space a field takes up (fills with 0 or spaces), MINIMUM
     precision: ?u32  = null, // amount of bytes depends on type
     conv: SubConversion,     // type to convert to
+    chars_parsed: u32 = 0,
 
-    pub fn parse(self: *Subsitution, arr: []const u8, index: u32) !u32 {
+    pub fn parse(self: *Subsitution, arr: []const u8, index: u32) !void {
         std.debug.assert(arr[index] == '%');
         var i: u32 = index;
         var ch: u8 = '%';
+        defer self.chars_parsed = i-index;
+        errdefer self.chars_parsed = i-index;
         // flags
         while (true) {
             ch = try getChar(arr, &i);
@@ -86,29 +90,45 @@ const Subsitution = struct {
             }
         }
         // field width
+        var buf: [256]u8 = undefined;
+        var bufindex: u8 = 0;
         while (true) {
             switch(ch) {
-                '0'...'9' => {},
+                '0'...'9' => {
+                    buf[bufindex] = ch;
+                    bufindex += 1;
+                },
                 else => break,
             }
             ch = try getChar(arr, &i);
         }
+        if (bufindex>0)
+            self.field_width = std.fmt.parseInt(u32, buf[0..bufindex], 10) catch return SubErr.InvalidConversion;
+
+        // self.field_width = std.enums.fromInt(SubConversion, fwarr) orelse return error.InvalidConversion;
+
         // precision
+        buf = .{0}**256;
+        bufindex=0;
         if (arr[i]=='.') {
             _  = try getChar(arr, &i); // eat the .
             ch = try getChar(arr, &i); // get the new input
             while (true) {
                 switch(ch) {
-                    '0'...'9' => {},
+                    '0'...'9' => {
+                        buf[bufindex] = ch;
+                        bufindex += 1;
+                    },
                     else => break,
                 }
                 ch = try getChar(arr, &i);
             }
         }
-        // conversion
-        self.conv = std.enums.fromInt(SubConversion, arr[i]) orelse return error.InvalidConversion;
+        if (bufindex>0)
+            self.precision = std.fmt.parseInt(u32, buf[0..bufindex], 10) catch return SubErr.InvalidConversion;
 
-        return i-index;
+        // conversion
+        self.conv = std.enums.fromInt(SubConversion, arr[i]) orelse return SubErr.InvalidConversion;
     }
 
     pub fn write(self: *Subsitution, writer: *std.Io.Writer, arg: ?[]const u8) SubErr!bool {
@@ -123,7 +143,7 @@ const Subsitution = struct {
             },
             .d, .i => {
                 if (arg) |a| {
-                    const int = std.fmt.parseInt(i64, a, 10) catch return SubErr.ParseInt; //fail(writer, "printf: %: failed to parse int", 3);
+                    const int = std.fmt.parseInt(i64, a, 10) catch return SubErr.ParseInt;
                     writer.print("{}", .{int}) catch return SubErr.OutOfMemory;
                 } else {
                     writer.print("0", .{}) catch return SubErr.OutOfMemory;
@@ -131,7 +151,7 @@ const Subsitution = struct {
             },
             .x => {
                 if (arg) |a| {
-                    const int = parseuint(a) catch return SubErr.ParseInt; //fail(writer, "printf: %: failed to parse int", 3);
+                    const int = parseuint(a) catch return SubErr.ParseInt;
                     writer.print("{x}", .{int}) catch return SubErr.OutOfMemory;
                 } else {
                     writer.print("0", .{}) catch return SubErr.OutOfMemory;
@@ -139,7 +159,7 @@ const Subsitution = struct {
             },
             .X => {
                 if (arg) |a| {
-                    const int = parseuint(a) catch return SubErr.ParseInt; //fail(writer, "printf: %: failed to parse int", 3);
+                    const int = parseuint(a) catch return SubErr.ParseInt;
                     writer.print("{X}", .{int}) catch return SubErr.OutOfMemory;
                 } else {
                     writer.print("0", .{}) catch return SubErr.OutOfMemory;
@@ -147,15 +167,15 @@ const Subsitution = struct {
             },
             .u => {
                 if (arg) |a| {
-                // -1 is supposed to give 18446744073709551615 :)
-                    const uint: u64 = parseuint(a) catch return SubErr.ParseUInt;//fail(writer, "printf: %: failed to parse uint (too long)", 3);
-                    writer.print("{}", .{uint}) catch return error.OutOfMemory;
+                    // -1 is supposed to give 18446744073709551615 :)
+                    const uint: u64 = parseuint(a) catch return SubErr.ParseUInt;
+                    writer.print("{}", .{uint}) catch return SubErr.OutOfMemory;
                 } else {
-                    writer.print("0", .{}) catch return error.OutOfMemory;
+                    writer.print("0", .{}) catch return SubErr.OutOfMemory;
                 }
             },
             .percent => {
-                writer.print("%", .{}) catch return error.OutOfMemory;
+                writer.print("%", .{}) catch return SubErr.OutOfMemory;
                 return false;
             },
             else => unreachable,
@@ -183,7 +203,7 @@ pub fn printf(vm: *rzvm, argv: []const []const u8) u8 {
     var outbuf: [1024]u8 = undefined;
     var errbuf: [1024]u8 = undefined;
     const fileno: std.Io.File = .stdout(); // @TODO(Renzix): handle pipe
-    const errno: std.Io.File = .stderr(); // @TODO(Renzix): handle pipe
+    const errno: std.Io.File = .stderr();  // @TODO(Renzix): handle pipe
     var file: std.Io.File.Writer = .init(fileno, vm.io, &outbuf);
     const writer = &file.interface;
     var errfile: std.Io.File.Writer = .init(errno, vm.io, &errbuf);
@@ -195,18 +215,27 @@ pub fn printf(vm: *rzvm, argv: []const []const u8) u8 {
         switch (format[index]) {
             '%' => { // % --- https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap05.html#tag_05
                 var sub: Subsitution = .{ .conv = .s };
-                const chars_parsed = sub.parse(format, index) catch return fail(errwriter, "printf: %: could not parse %", 3);
+                sub.parse(format, index) catch {
+                    fail(errwriter, "printf: %: invalid directive");
+                    break;
+                };
                 if (currarg>=argv.len) {
-                    _ = sub.write(writer, null) catch return fail(errwriter, "printf: %: could not write % value", 3);
+                    _ = sub.write(writer, null) catch fail(errwriter, "printf: %: could not write % value");
                 } else {
-                    const used_arg = sub.write(writer, argv[currarg]) catch return fail(errwriter, "printf: %: could not write % value", 3);
+                    const used_arg = sub.write(writer, argv[currarg]) catch blk: {
+                        fail(errwriter, "printf: %: could not write % value");
+                        break :blk true;
+                    };
                     if (used_arg) currarg += 1;
                 }
-                index += chars_parsed;
+                index += sub.chars_parsed;
             },
             '\\' => {
                 index+=1;
-                if (index>=format.len) return fail(errwriter, "printf: \\: tried to \\ when nothing is next", 4);
+                if (index>=format.len) {
+                    fail(errwriter, "printf: \\: tried to \\ when nothing is next");
+                    return 4;
+                }
                 const byte: ?u8 = switch (format[index]) {
                     '\\' => '\\',
                     'a' => '\x07',
@@ -230,9 +259,7 @@ pub fn printf(vm: *rzvm, argv: []const []const u8) u8 {
     return 0;
 }
 
-fn fail(writer: *std.Io.Writer, msg: []const u8, ret: u8) u8 {
-    writer.print("{s}\n", .{msg}) catch return ret;
-    writer.flush() catch return ret;
-    return ret;
+fn fail(writer: *std.Io.Writer, msg: []const u8) void {
+    writer.print("{s}\n", .{msg}) catch return;
+    writer.flush() catch return;
 }
-
